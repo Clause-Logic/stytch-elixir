2fe4885db92ec86d87cc95676b26151feb880915
Stephen Solka <stephen@solka.dev>
2023-10-16 15:41:16 -0400

    allow multiple configs

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 import Config
 
-config :stytch,
+config :stytch, Stytch.DefaultProject,
   endpoint: System.get_env("STYTCH_ENDPOINT", "https://test.stytch.com/v1"),
   project_id: System.get_env("STYTCH_PROJECT_ID"),
   secret: System.get_env("STYTCH_SECRET")
---
+++ lib/stytch/default_project.ex
@@ -0,0 +1,7 @@
+defmodule Stytch.DefaultProject do
+  @moduledoc """
+  Default project configuration for Stytch.
+  """
+
+  use Stytch.Project, :stytch
+end
